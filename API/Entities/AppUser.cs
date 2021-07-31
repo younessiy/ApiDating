1a3d5d23a20f9c50002c31c5f02a72167dad3e91
younessiysa <youness.iysa@gmail.com>
2021-07-31 01:26:34 +0100

    refactoring database (identity) and role managment

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,15 +1,12 @@
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
-using API.Extensions;
 
 namespace API.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
+      
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
@@ -26,5 +23,6 @@ namespace API.Entities
         public ICollection<UserLike> LikedUsers { get; set; }
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
